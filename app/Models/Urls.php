89a31592da8dcdfcbc2b4cb0f132e05ed1eee14a
Autovot <87210193+Autovot@users.noreply.github.com>
2024-02-08 01:08:52 +0100

    Add Urls model and migration

---
+++ app/Models/Urls.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Urls extends Model
+{
+    use HasFactory;
+
+    protected $table = 'url';
+
+    protected $fillable = [
+        'origin',
+        'smashed',
+        'used'
+    ];
+}
