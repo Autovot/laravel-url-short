54573f389574691fc0592695e1c780d693d02d98
Autovot <87210193+Autovot@users.noreply.github.com>
2024-02-10 00:24:54 +0100

    Add test for shortener API endpoint

--- tests/Feature/shortenerTest.php
+++ tests/Feature/shortenerTest.php
@@ -2,34 +2,42 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Testing\TestResponse;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ShortenerTest extends TestCase
 {
+
+    // Reset database
+    use RefreshDatabase;
+
     /**
      * Endpoint /shortener de tipo post que recibe una url
      * y devuelve una url acortada.
      */
-    public function test_shortener_api(): void
+    public function test_shortener_api(): bool
     {
-
         $urlInput = 'https://www.google.es/';
+
+        $urlOutputTest = 'https://localhost/url/' . md5($urlInput);
+
         $response = $this->postJson('/api/shortener', ['url-input' => $urlInput]);
 
-        $response
-            ->assertStatus(200)
-            ->assertJson([
-                'url-input' => $urlInput
-            ]);
+        $urlOutputApi = 'https://localhost/url/' . $response->json()['smashed'];
+
+        $result = $urlOutputTest == $urlOutputApi;
+        var_dump($result);
+        return $result;
     }
 
-    public function test_short_url(): void
-    {
+    // public function test_short_url(): void
+    // {
 
-    }
+    // }
 
-    public function test_short_url_get_data()
-    {
+    // public function test_short_url_get_data()
+    // {
 
-    }
+    // }
 }
